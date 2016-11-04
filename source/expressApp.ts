111226914b9b9abe4806a0a2296b32d5f62f562e
SamGraber <authorsamgraber@gmail.com>
2016-11-04 16:09:34 -0400

    Added a body parser

--- source/expressApp.ts
+++ source/expressApp.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 export class ExpressApplication {}
 
 export const expressApp: express.Application = express();
+expressApp.use(bodyParser.json());
