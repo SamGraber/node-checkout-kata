fc28c2b7702044b0fa0de1f5f423174cc7b97aa4
SamGraber <authorsamgraber@gmail.com>
2016-11-07 13:38:15 -0500

    Load up the offers with http

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
 	moduleId: module.id,
 	selector: 'my-app',
 	templateUrl: 'app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	offers: any[];
+	
+	constructor(private http: Http) {}
 
+	ngOnInit(): void {
+		this.http.get('http://localhost:3000/api/offer')
+				.map(response => response.json())
+				.subscribe(data => this.offers = data);
+	}
 }
